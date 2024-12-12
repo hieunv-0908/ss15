43a3c2d7983a99d412ad94beac58519bc791c3b1
hieunv-0908 <nn650370@gmail.com>
2024-12-12 16:19:56 +0700

    Update Sission15-Ex07-B24DTCN222.c

--- Sission15-Ex07-B24DTCN222.c
+++ Sission15-Ex07-B24DTCN222.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 void count(char string[]){
-	int count=0;
+	int alphabet=0;
+	int num=0;
+	int specialCharacters=0;
 	for(int i=0;string[i] !='\0';i++){
-		if(string[i]!= ' '&& string[i]!='\n'){
-			count++;
+		if((string[i] >='A'&&string[i] <='Z') || (string[i] >='a'&&string[i] <='z')){
+			alphabet++;
+		}else if((string[i] >='0'&&string[i] <='9')){
+			num++;
+		}else if(string[i]!= ' '&& string[i]!='\n'){
+			specialCharacters++;
 		}
 	}
-	printf("quatity characters in string:%d",count);
+	printf("quatity characters is alphabet:%d\n",alphabet);
+	printf("quatity characters is number:%d\n",num);
+	printf("quatity special characters :%d\n",specialCharacters);
+	
 }
 
 int main(){
